a2c7882d013dfa5b57260e295c742f2235124cbf
hyeon <selwha1@naver.com>
2024-06-27 13:09:37 +0900

    한국어 대화 다중분류

---
+++ 16_mian_quest_DLThon/README.md
@@ -0,0 +1,7 @@
+# DKMC 데이터 다중 분류
+
+[공유 git](https://github.com/silpiria98/aiffel_camp/tree/main/aiffel_main/DLThon1)
+
+[notion 기록](https://www.notion.so/modulabs/3-4eb0d20908bb42b385238de62c5009cc)
+
+[발표 ppt](https://docs.google.com/presentation/d/1iNqWK_QTKmBllqsRsPYmJl53_8H4ZOuwvdzO9EgEQGg/edit#slide=id.g2e80eea5ab4_0_0)
---
+++ 16_mian_quest_DLThon/preprocessing.py
@@ -0,0 +1,68 @@
+#전처리
+# 불용어 처리를 할 경우 stopwords에 단어 리스트를 제공
+
+# sample stopwords
+# ['그렇게','아','어떻게', '이렇게', '그렇군요', '있어요',
+# '내가', '다', '니','니가','넌','그냥', '너', '왜', '야','진짜',
+# '나','좀', '지금', '내', '아니','우리','네','안','그','이','어',
+# '그래', '그럼', '아니야', '응', '너가', '제가', '저', '거','뭐',
+# '이거','여기', '저는','저도', '전', '어', '나도', '잘', '너무',
+# '정말', '나는', '너도', '네가', '넌', '난', '널',]
+
+# 워드 클라우드에서 보기 편하게 일반어 제거
+
+import re
+
+def preprocess_sentence(sentence, stopwords=None):
+    # 개행자 삭제
+    sentence = re.sub(r'[\n\r]', ' ', sentence)
+    
+    # 단어와 구두점(punctuation) 사이의 거리를 만듭니다.
+    # 예를 들어서 "I am a student." => "I am a student ."와 같이
+    # student와 온점 사이에 거리를 만듭니다. 
+    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    sentence = re.sub(r'[" "]+', " ", sentence)
+
+    # (a-z, A-Z,가-힣,0-9, ".", "?", "!", ",")를 제외한 모든 문자를 공백인 ' '로 대체합니다.
+    sentence = re.sub(r"[^a-zA-Z가-힣0-9\.\?\!,]", " ", sentence)
+    sentence = sentence.strip()
+
+    # '키키'와 같이 연속된 키를 제거합니다.
+    sentence = re.sub(r'키{2,}', '', sentence)
+    sentence = re.sub(r'\b키\b', '', sentence)
+
+    if stopwords:
+        words = sentence.split()
+        filtered_words = [word for word in words if word not in stopwords]
+        sentence = ' '.join(filtered_words)
+
+    return sentence
+
+def augmentation(data):
+    import pandas as pd
+    def split_text(text):
+        half_index = len(text) // 2
+        return text[:half_index], text[half_index:]
+    
+    new_data = {
+        'idx': [],
+        'class': [],
+        'conversation': [],
+        'label': []
+        }
+
+    for idx, row in data.iterrows():
+        part1, part2 = split_text(row['conversation'])
+        new_data['idx'].append(row['idx'])
+        new_data['class'].append(row['class'])
+        new_data['conversation'].append(part1)
+        new_data['label'].append(row['label'])
+        
+        new_data['idx'].append(row['idx'])
+        new_data['class'].append(row['class'])
+        new_data['conversation'].append(part2)
+        new_data['label'].append(row['label'])
+
+    new_df = pd.DataFrame(new_data)
+    
+    return new_df
